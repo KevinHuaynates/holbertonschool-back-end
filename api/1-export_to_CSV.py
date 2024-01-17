3d1790f38406c6f6cbece922f1d560e0340451f2
KevinHuaynates <6992@holbertonstudents.com>
2024-01-16 22:17:00 -0800

    2

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-Script to gather data from a REST API and display TODO list progress for a given employee ID.
+Script to gather data from a REST API and export TODO list progress to CSV for a given employee ID.
 """
 
 import requests
 import sys
+import csv
 
 def get_employee_data(employee_id):
     """
@@ -25,30 +26,35 @@ def get_employee_data(employee_id):
         print(f"Error: Unable to fetch data for employee ID {employee_id}")
         sys.exit(1)
 
-def display_todo_progress(employee_id, todo_data):
+def export_to_csv(employee_id, todo_data):
     """
-    Display TODO list progress for a given employee.
+    Export TODO list progress to CSV for a given employee.
 
     Args:
         employee_id (int): The ID of the employee.
         todo_data (list): A list containing data about the employee's TODO list.
     """
-    completed_tasks = [task for task in todo_data if task['completed']]
-
     employee_name = todo_data[0]['username']
-    total_tasks = len(todo_data)
-    completed_task_count = len(completed_tasks)
+    csv_filename = f"{employee_id}.csv"
 
-    print(f"Employee {employee_name} is done with tasks({completed_task_count}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
+    with open(csv_filename, mode='w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+
+        # Write header
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+        # Write data
+        for task in todo_data:
+            csv_writer.writerow([employee_id, employee_name, str(task['completed']), task['title']])
+
+    print(f"Data exported to {csv_filename}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+        print("Usage: python3 1-export_to_CSV.py <employee_id>")
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
     employee_todo_data = get_employee_data(employee_id)
-    display_todo_progress(employee_id, employee_todo_data)
+    export_to_csv(employee_id, employee_todo_data)
 
