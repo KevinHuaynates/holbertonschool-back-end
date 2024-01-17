b4cbcd15ea98e85a54806bab19b16ce8c2ff28ad
KevinHuaynates <6992@holbertonstudents.com>
2024-01-16 22:14:53 -0800

    1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,48 +1,54 @@
 #!/usr/bin/python3
+"""
+Script to gather data from a REST API and display TODO list progress for a given employee ID.
+"""
+
 import requests
 import sys
 
-
 def get_employee_data(employee_id):
-    # URL de la API proporcionada
-    api_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    """
+    Get employee data from the API for a given employee ID.
 
-    # Realizar la solicitud GET a la API
+    Args:
+        employee_id (int): The ID of the employee.
+
+    Returns:
+        list: A list containing data about the employee's TODO list.
+    """
+    api_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
     response = requests.get(api_url)
 
-    # Verificar si la solicitud fue exitosa (código 200)
     if response.status_code == 200:
         return response.json()
     else:
         print(f"Error: Unable to fetch data for employee ID {employee_id}")
         sys.exit(1)
 
-
 def display_todo_progress(employee_id, todo_data):
-    # Filtrar tareas completadas
+    """
+    Display TODO list progress for a given employee.
+
+    Args:
+        employee_id (int): The ID of the employee.
+        todo_data (list): A list containing data about the employee's TODO list.
+    """
     completed_tasks = [task for task in todo_data if task['completed']]
 
-    # Obtener información para el formato de salida
     employee_name = todo_data[0]['username']
     total_tasks = len(todo_data)
     completed_task_count = len(completed_tasks)
 
-    # Imprimir el progreso de la lista de tareas
     print(f"Employee {employee_name} is done with tasks({completed_task_count}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t{task['title']}")
 
 if __name__ == "__main__":
-    # Verificar si se proporciona el ID del empleado como argumento
     if len(sys.argv) != 2 or not sys.argv[1].isdigit():
         print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
-    # Obtener el ID del empleado desde el argumento de línea de comandos
     employee_id = int(sys.argv[1])
-
-    # Obtener datos de la API
     employee_todo_data = get_employee_data(employee_id)
-
-    # Mostrar el progreso de la lista de tareas
     display_todo_progress(employee_id, employee_todo_data)
+
