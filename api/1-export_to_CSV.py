ae3fff44744959e76e7cbd7e7cdce987cda32b7b
KevinHuaynates <6992@holbertonstudents.com>
2024-01-29 21:51:33 -0800

    2

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,54 +1,50 @@
 #!/usr/bin/python3
 """
-Export employee TODO list progress to CSV format
+Extend your Python script to export data in CSV format.
 """
-
 import csv
 import requests
 from sys import argv
 
-if __name__ == "__main__":
-    if len(argv) != 2 or not argv[1].isdigit():
-        print("Usage: {} <employee_id>".format(argv[0]))
+
+def export_to_csv(user_id):
+    api_url = "https://jsonplaceholder.typicode.com"
+    users = requests.get(f"{api_url}/users/{user_id}").json()
+    todo_data = requests.get(f"{api_url}/todos?userId={user_id}").json()
+
+    if not users or not todo_data:
+        print("User or tasks not found.")
         exit(1)
 
-    employee_id = int(argv[1])
-    api_url = "https://jsonplaceholder.typicode.com"
+    username = users["username"]
+    filename = f"{user_id}.csv"
 
-    # Get user data
-    user_response = requests.get(
-        "{}/users/{}".format(api_url, employee_id)
-    )
-    user_data = user_response.json()
-    user_id = user_data.get("id")
-    user_name = user_data.get("username")
-
-    # Get TODO list
-    todo_response = requests.get(
-        "{}/todos?userId={}".format(api_url, employee_id)
-    )
-    todo_data = todo_response.json()
-
-    # Filter tasks owned by the user
-    user_tasks = [task for task in todo_data if task['userId'] == employee_id]
-
-    # Export to CSV
-    csv_file_name = "{}.csv".format(user_id)
-    with open(csv_file_name, mode='w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        csv_writer.writerow([
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = [
             "USER_ID",
             "USERNAME",
             "TASK_COMPLETED_STATUS",
             "TASK_TITLE"
-        ])
-        for task in user_tasks:
-            csv_writer.writerow([
-                user_id,
-                user_name,
-                str(task.get("completed")),
-                task.get("title")
-            ])
+        ]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    # Print number of tasks
-    print("Number of tasks in CSV: {}".format(len(user_tasks)))
+        writer.writeheader()
+
+        for task in todo_data:
+            writer.writerow({
+                "USER_ID": user_id,
+                "USERNAME": username,
+                "TASK_COMPLETED_STATUS": str(task["completed"]),
+                "TASK_TITLE": task["title"]
+            })
+
+    print(f"CSV file '{filename}' created.")
+
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: {} <user_id>".format(argv[0]))
+        exit(1)
+
+    user_id = argv[1]
+    export_to_csv(user_id)
