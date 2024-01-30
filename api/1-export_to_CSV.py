9fb177ea3c59044a64c979990768a600b7de0369
KevinHuaynates <6992@holbertonstudents.com>
2024-01-29 22:21:55 -0800

    1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,55 +1,64 @@
 #!/usr/bin/python3
 """
-Module para exportar datos de tareas de un usuario en formato CSV
+Script to export user tasks data in CSV format.
+
+Requirements:
+- Records all tasks that are owned by this user.
+- Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+- File name must be: USER_ID.csv
 """
 
 import csv
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
-    # Obtener el ID del usuario del argumento de línea de comandos
+    if len(argv) != 2 or not argv[1].isdigit():
+        print("Usage: {} USER_ID".format(argv[0]))
+        exit(1)
+
     user_id = argv[1]
+    user_info_url = (
+        'https://jsonplaceholder.typicode.com/users/{}'
+        .format(user_id)
+    )
+    tasks_url = (
+        'https://jsonplaceholder.typicode.com/todos?userId={}'
+        .format(user_id)
+    )
+    user_response = requests.get(user_info_url)
+    tasks_response = requests.get(tasks_url)
 
-    # Construir las URLs para obtener información del usuario y tareas
-    user_info_url =
-    'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    tasks_url =
-    'https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id)
+    if user_response.status_code != 200:
+        print("User not found")
+        exit(1)
 
-    # Obtener datos del usuario y tareas
-    response_user = requests.get(user_info_url)
-    response_tasks = requests.get(tasks_url)
+    user_data = user_response.json()
+    tasks_data = tasks_response.json()
 
-    user_data = response_user.json()
-    tasks_data = response_tasks.json()
-
-    # Obtener el nombre de usuario del objeto de datos del usuario
-    username = user_data.get('username')
-
-    # Construir el nombre del archivo CSV
+    username = user_data.get('username', 'Unknown')
     filename = '{}.csv'.format(user_id)
 
-    # Abrir el archivo CSV para escribir
-    with open(filename, mode='w', newline='') as csvfile:
-        # Definir los nombres de campo para el encabezado CSV
-        fieldnames = ["USER_ID",
-                      "USERNAME",
-                      "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        # Crear un escritor CSV
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    with open(filename, mode='w', newline='', encoding='utf-8') as csv_file:
+        fieldnames = [
+            "USER_ID",
+            "USERNAME",
+            "TASK_COMPLETED_STATUS",
+            "TASK_TITLE"
+        ]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
-        # Escribir el encabezado
         writer.writeheader()
-
-        # Iterar sobre las tareas y escribir cada tarea en una fila CSV
         for task in tasks_data:
             writer.writerow({
                 "USER_ID": user_id,
                 "USERNAME": username,
-                "TASK_COMPLETED_STATUS":
-                "True" if task["completed"] else "False",
+                "TASK_COMPLETED_STATUS": (
+                    "True" if task["completed"]
+                    else "False"
+                ),
                 "TASK_TITLE": task["title"]
             })
 
-    print("Number of tasks in CSV: OK")
+    print("CSV file '{}' created.".format(filename))
